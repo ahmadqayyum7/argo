d3eceefddab95c6652bf096b42f8d39bc5cd6c84
ahmadqayyum7 <ahmadqayyum19@gmil.com>
2025-03-12 02:55:47 +0500

    updated namspace to new one

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployed by CI/CD.
+          Deployed by CI/CD to ArgoCd.
         </p>
         <a
           className="App-link"
