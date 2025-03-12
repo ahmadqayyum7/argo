4a4b0d8b1b711b32d371b4330ac7a8f11a7ce5c5
ahmadqayyum7 <ahmadqayyum19@gmil.com>
2025-03-12 22:40:09 +0500

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployed by CI/CD to ArgoCd and testing with image tags.
+          Test1.
         </p>
         <a
           className="App-link"
